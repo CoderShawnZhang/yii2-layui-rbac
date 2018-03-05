3d06a690b3bdca8962cdc0acad2450ead2bf98f1
张洪波 <zhanghongbo@anlewo.com>
2018-03-05 10:55:29 +0800

    Init prom

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anlewo0208
+ * Date: 2018/3/5
+ * Time: 上午10:55
+ */
+
+namespace shawn\rbac;
+
+class index
+{
+    public static function index()
+    {
+        echo 123;
+    }
+}
